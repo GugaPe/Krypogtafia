eac0179ba07a8c82ce50a764a6e81f855983582a
GugaPe <78729857+GugaPe@users.noreply.github.com>
2022-05-27 13:32:19 +0200

    Update serwerus.py

--- serwerus.py
+++ serwerus.py
@@ -1,44 +1,149 @@
 import socket
+from _thread import *
+import os
+import uuid
+import termcolor
 
-    
-if __name__ == '__main__':
-    s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s1.bind(('localhost', 2137))
-    s1.listen(5)
-    
-    while True:
-        client1, addr = s1.accept()
-        client1.send((f'CONNECTED WITH {addr[0]}').encode())
-        client1.close()
-        break
-    
-    s1.close()
-    s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s2.bind(('localhost', 1137))
-    s2.listen(5)
-    
-    while True:
-        client2, addr = s2.accept()
-        while True:
-            data = client2.recv(2048)
-            command = data.decode()
+ip_connection = ('localhost', 2137)
+
+def send_response(socket_connection, status_code, data):
+    code_description = ''
+
+    if status_code == 200:
+        os.system('color 20')
+        code_description = termcolor.colored("Sucess!", "green")
+    elif status_code == 210:
+        os.system('color 20')
+        code_description = termcolor.colored("No messages", "green")
+    elif status_code == 300:
+        os.system('color 40')
+        code_description = termcolor.colored("Wrong command", "red")
+    elif status_code == 404:
+        os.system('color 40')
+        code_description = termcolor.colored("Wrong params for command", "red")
+    elif status_code == 500:
+        os.system('color 40')
+        code_description = termcolor.colored("You are not authorised user.", "red")
+    else:
+        os.system('color 0')
+        code_description = termcolor.colored("Something went wrong! Unexpected error.", "yellow")
+        
+    socket_connection.sendall((str(status_code) + '\r\n' + code_description +'\r\n' + data + '\r\n\r\n').encode('utf-8'))
+
+
+users = {'guga': 'xd', 'gugu': 'xdd'}
+
+auth_users = []
+
+messages = []
+
+def server_client(client):
+    global userMessage
+    data = ''
+    while 'END' not in data:
+        data = b''
+        while not b'\r\n\r\n' in data:
+            data += client.recv(1)
+        data = data.decode('utf-8')
+        print(data,'\r\n')
+        if 'LOGIN' in data:
+            params = data.split("\r\n")
             
-            if not command:
-                break
-            user_list = ['user:gustaw-password:xd']
+            if params[1] in users and users[params[1]] == params[2]:
+                session_id = uuid.uuid4().hex
+                auth_users.append(session_id)
+                send_response(client, 200, session_id)
+            else:
+                send_response(client, 404, "User or pass is wrong!")
             
-            if str(command) == 'END':
-                client2.send(b'closing')
-                client2.close()
-                break
+        elif 'MESSAGE' in data:
+            if 'session' in data:
+                session = data.split('session:')[1].split('\r\n')[0]
+                print(session)
+            else:
+                session = False
+            params = data.split("\r\n")
+            
+            if session_id == False or session not in auth_users:
+                send_response(client, 500, "Please log in.")
+            
+            elif params[1] == params[2] == '' or params[1] not in users:
+                    send_response(client, 404, "User doesnt exist")
+            
+            else:
+                messages.append( ( params[1], params[2]) )
+                send_response(client, 200, "Sent!")
+                print(messages)
                 
-            elif str(command) == 'LOGIN':
-                client2.send(b'Username:')
-                user = client2.recv(2048).decode()
-                client2.send(b'Password:')
-                password = client2.recv(2048).decode()
                 
-                if f'user:{user}-password:{password}' in user_list:
-                    client2.send(b'You have succesfully logged in!')
+        elif 'SHOW' in data:
+            if 'session' in data:
+                session = data.split('session:')[1].split('\r\n')[0]
+                print(session)
+            else:
+                session = False
+            params = data.split("\r\n")
+            print(data)
+            userMessage = ''
+            for i in messages:
+                if i[0] == params[1]:
+                    userMessage = userMessage + f'  {i[0]}:{i[1]}  ||'
+            if userMessage != '':
+                print(f"your messages {userMessage}")
+                if session_id == False or session not in auth_users:
+                    send_response(client, 500, "Please log in.")
+                
                 else:
-                    client2.send(b'wrong username or password!')
+                    send_response(client, 200, userMessage)
+            else:
+                send_response(client, 210, 'You have no messages!')
+                
+                
+        elif 'CLEAR' in data:
+            if 'session' in data:
+                session = data.split('session:')[1].split('\r\n')[0]
+                print(session)
+            else:
+                session = False
+            params = data.split("\r\n")
+            print(data)
+            x = 0
+            if messages != []:
+                if session_id == False or session not in auth_users:
+                    send_response(client, 500, "Please log in.")
+
+                else:
+                    #TODO: czemu nie działa for i???
+                    print(messages)
+                    while x<1:
+                        print('1')
+                        if 1==1:
+                            x=x+1
+                            print('2')
+                        print('3')
+                    print('4')
+                    for i in messages:
+                        print(i)
+                        if i[0] == params[1]:
+                            messages.pop(x)
+                        x = x+1
+                    send_response(client, 200, 'Messages deleted!')
+            else:
+                send_response(client, 210, 'You have no messages to delete!')
+            
+
+    client.close()
+
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(ip_connection)
+    s.listen(5)
+
+    try:
+        while True:
+            client, addr = s.accept()
+            print("CONNECTED WITH ", addr[0])
+            start_new_thread(server_client, (client, ))
+    except KeyboardInterrupt:
+        s.close()
